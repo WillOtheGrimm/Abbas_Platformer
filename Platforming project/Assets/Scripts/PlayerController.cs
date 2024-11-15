49e6902c8feb15a4cc13cfd688cc26a8623157a0
WillOtheGrimm <ayman010905@gmail.com>
2024-11-15 02:37:32 -0500

    Finished Task 1 & 2

--- Platforming project/Assets/Scripts/PlayerController.cs
+++ Platforming project/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpHeight;
+    FacingDirection currentDirection;
     public enum FacingDirection
     {
         left, right
@@ -12,7 +17,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -22,24 +28,62 @@ public class PlayerController : MonoBehaviour
         //manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
         MovementUpdate(playerInput);
+
+
+        Debug.Log(rb.gravityScale);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        float inputX = Input.GetAxis("Horizontal");
+        rb.AddForce(new Vector2(inputX * speed, 0));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("jump");
+            rb.AddForce(new Vector2(0, 1 * jumpHeight));
+
+        }
+
+        
+
+        
 
     }
 
     public bool IsWalking()
     {
-        return false;
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            return true;
+        }
+        else return false;
     }
+
+
+
     public bool IsGrounded()
     {
-        return false;
+        return true;
     }
+
+
+
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            currentDirection = FacingDirection.right;
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            currentDirection= FacingDirection.left;
+        }
+         return currentDirection;
+
+
+
     }
 }
